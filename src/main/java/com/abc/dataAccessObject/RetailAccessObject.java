58e049f829198eb15224db449a9e7866a192750f
inaeon <github@inaeon.com>
2025-03-30 18:25:03 +0100

    Additional comments deleted

--- src/main/java/com/abc/dataAccessObject/RetailAccessObject.java
+++ src/main/java/com/abc/dataAccessObject/RetailAccessObject.java
@@ -5,13 +5,6 @@ import com.abc.RetailModule;
 public interface RetailAccessObject {
 	void create(RetailModule product);
 	RetailModule read(int product_id);
-	// comment1
-	// comment2 demo compile
-	// demo with compile and test goals
-	// demo compile, test, and package goals
-	// one more trial
-	// trial 2
-	// Final Trial 
-	// after jenkinsfile update
+	
 	
 }
